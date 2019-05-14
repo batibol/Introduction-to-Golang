a27beb91fdc0a700e3ea0b7c457a0d9faeb72f1f
Dave Wade-Stein <dws@developintelligence.com>
2019-05-13 19:52:30 -0700

    initial commit

---
+++ src/ranveer.go
@@ -0,0 +1,13 @@
+package main
+
+import "time"
+import "fmt"
+
+// START OMIT
+func main() {
+    name, almaMater := "Ranveer Singh", "Indiana University"
+    age := 33
+    println(name, age, almaMater)
+    fmt.Println(time.Now())
+}
+// END OMIT
